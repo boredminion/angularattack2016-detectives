90c12b9ac815b0e30aa448126b30efaae681d0f3
David Mulhern <david@mulhern.me>
2016-05-13 21:34:32 -0700

    Resetting project after deploy issue.

--- app/bootstrap-web.ts
+++
@@ -1,5 +0,0 @@
-import {bootstrap} from '@angular/platform-browser-dynamic';
-import {DetectivesApp} from './detectives-app.component';
-
-bootstrap(DetectivesApp)
- .catch(err => console.log(err));
--- app/detectives-app.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-	selector: 'detectives-app',
-	template: '<pre>Detectives</pre>'
-})
-export class DetectivesApp {
-	
-}
