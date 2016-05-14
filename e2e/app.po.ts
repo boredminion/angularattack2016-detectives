49d9fe18e25cf2f3fba555276fb760b570f39c72
David Mulhern <david@mulhern.me>
2016-05-14 16:30:40 -0700

    Removing angular-cli.

--- e2e/app.po.ts
+++
@@ -1,9 +0,0 @@
-export class DetectivesPage {
-  navigateTo() {
-    return browser.get('/');
-  }
-
-  getParagraphText() {
-    return element(by.css('detectives-app p')).getText();
-  }
-}
--- src/app/index.ts
+++
@@ -1,2 +0,0 @@
-export {environment} from './environment';
-export {DetectivesAppComponent} from './detectives.component';
