a105be073386fc4099e4a7f9d59528902f5d1ea3
David Mulhern <david@mulhern.me>
2016-05-15 16:33:42 -0700

    Adding the doug prefix now that there is more than one sprite.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import {DougComponent} from './doug.component';
-import {BubbleComponent} from './bubble.component';
+import {BubbleComponent} from './doug-bubble.component';
 
 import '../../public/css/styles.css';
 @Component({
--- src/app/doug-bubble.component.ts
+++ src/app/doug-bubble.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'bubble-sprite',
-  template: require('./bubble.component.html'),
-  styles: [require('./bubble.component.css')]
+  template: require('./doug-bubble.component.html'),
+  styles: [require('./doug-bubble.component.css')]
 })
 export class BubbleComponent { 
 	
