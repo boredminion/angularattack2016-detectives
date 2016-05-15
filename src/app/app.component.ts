8c31f24ca503c79b6b19e381e64b58cc5ab0808d
David Mulhern <david@mulhern.me>
2016-05-15 16:15:26 -0700

    added Bubble

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import {DougComponent} from './doug.component';
+import {BubbleComponent} from './bubble.component';
 
 import '../../public/css/styles.css';
 @Component({
   selector: 'doug-app',
   template: require('./app.component.html'),
   styles: [require('./app.component.css')],
-  directives: [DougComponent]
+  directives: [DougComponent,BubbleComponent]
 })
 export class AppComponent { 
 	
---
+++ src/app/bubble.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'bubble-sprite',
+  template: require('./bubble.component.html'),
+  styles: [require('./bubble.component.css')]
+})
+export class BubbleComponent { 
+	
+}
